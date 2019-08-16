a20292d017e0e0e7d14976858032a4888cd4d653
Abu Nasar <amitkashyap01@yahoo.co.in>
2019-08-17 09:04:19 +1000

    Initial commit

---
+++ JavaPerformanceDemo/src/main/java/com/performance/demo/JavaPerformanceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.performance.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPerformanceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPerformanceDemoApplication.class, args);
+	}
+
+}
